8a87516f92fb543cb29765231b89d43c8d80efd8
Sylvain Boily <sboily@proformatique.com>
2015-02-10 12:04:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+xivo-confgend-client
+====================
+
+[![Build Status](https://travis-ci.org/xivo-pbx/xivo-confgend-client.png?branch=master)](https://travis-ci.org/xivo-pbx/xivo-confgend-client)
+
+xivo-confgend-client is a client for xivo-confgend service for generating IPBX configuration files
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import fnmatch
+import os
+
+
+def is_package(path):
+    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
+    is_test_module = fnmatch.fnmatch(path, '*tests')
+    return not (is_svn_dir or is_test_module)
+
+packages = [p for p, _, _ in os.walk('xivo_confgen') if is_package(p)]
+
+
+setup(
+    name='xivo-confgend-client',
+    version='0.1',
+    description='XIVO Configurations Generator client',
+    author='Avencall',
+    author_email='xivo-dev@lists.proformatique.com',
+    url='http://www.xivo.io/',
+    license='GPLv3',
+    packages=packages,
+    scripts=['bin/xivo-confgen'],
+    data_files=[('/etc/xivo', ['etc/xivo-confgen.conf'])],
+)
