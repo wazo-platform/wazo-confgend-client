2683cbed7ef7238723ae4972e6759486611652d3
Sebastien Duthil <sduthil@avencall.com>
2015-02-17 10:09:48 -0500

    simplify debian packaging

--- setup.py
+++ setup.py
@@ -2,17 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
-import fnmatch
-import os
-
-
-def is_package(path):
-    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
-    is_test_module = fnmatch.fnmatch(path, '*tests')
-    return not (is_svn_dir or is_test_module)
-
-packages = [p for p, _, _ in os.walk('xivo_confgen') if is_package(p)]
-
 
 setup(
     name='xivo-confgend-client',
@@ -22,7 +11,6 @@ setup(
     author_email='xivo-dev@lists.proformatique.com',
     url='http://www.xivo.io/',
     license='GPLv3',
-    packages=packages,
     scripts=['bin/xivo-confgen'],
     data_files=[('/etc/xivo-confgend-client', ['etc/xivo-confgend-client/config.conf'])],
 )
