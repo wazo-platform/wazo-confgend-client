35615c9dd1c87b999f8a0f54b423e6b4b26f6327
Etienne Lessard <elessard97@gmail.com>
2016-12-29 16:17:10 -0500

    update for Wazo

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     name='xivo-confgend-client',
     version='0.1',
     description='XIVO Configurations Generator client',
-    author='Avencall',
-    author_email='xivo-dev@lists.proformatique.com',
-    url='http://www.xivo.io/',
+    author='Wazo Authors',
+    author_email='dev.wazo@gmail.com',
+    url='http://wazo.community',
     license='GPLv3',
     scripts=['bin/xivo-confgen'],
 )
