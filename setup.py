e97da089db1426c1a3582bce1e08980dc1f5ae23
Pascal Cadotte Michaud <pcm@wazo.io>
2019-09-18 15:24:11 -0400

    rename to wazo-confgend-client

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from setuptools import setup
 
 setup(
-    name='xivo-confgend-client',
+    name='wazo-confgend-client',
     version='0.1',
-    description='XIVO Configurations Generator client',
+    description='wazo-confgend CLI client',
     author='Wazo Authors',
-    author_email='dev.wazo@gmail.com',
-    url='http://wazo.community',
+    author_email='dev@wazo.community',
+    url='http://wazo-platform.org',
     license='GPLv3',
-    scripts=['bin/xivo-confgen'],
+    scripts=['bin/wazo-confgen'],
 )
